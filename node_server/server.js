858abcb891e470e7d0b14010ae196c04fe517909
Alex Kubica <me@alexkubica.com>
2019-09-18 20:41:08 +0300

    add mongo query and nodemon to restart server on file changes

--- node_server/server.js
+++ node_server/server.js
@@ -7,34 +7,76 @@ const PORT = Number(process.env.PORT || 8080);
 const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://localhost:27017';
 const MONGO_DB_NAME = process.env.MONGO_DB_NAME || 'jarvis-db';
 
-const app = express()
+// Mongo collections
+const DISTANCES_COLLECTION = 'distances';
+const UVS_COLLECTION = 'uvs';
+const HEARTS_COLLECTION = 'hearts';
+const TILTS_COLLECTION = 'tilts';
+const BOTTLES_COLLECTION = 'bottles';
 
-// Use connect method to connect to the server
-MongoClient.connect(MONGO_DB_URL,
+const app = express()
+const mongoClient = new MongoClient(
+  MONGO_DB_URL,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
-  },
-  function (err, client) {
-    if (client) {
-      console.log("Connected successfully to db");
+  }
+);
 
-      app.get('/', function (req, res) {
-        res.send('Hello World')
-      })
-      app.get('/kaki', function (req, res) {
-        res.send('Hello World');
-        console.log("/kaki");
-      })
-      
-      app.listen(PORT, '0.0.0.0', () => {
-        console.log(`HTTP server started on port: ${PORT}`)
-      })
+app.get('/', (req, res) => {
+  res.send(`Welcome to jarvis' node server!`)
+});
 
-      // const db = client.db(MONGO_DB_NAME);
-
-      // client.close();
+app.get('/distance', (req, res) => {
+  mongoClient.connect(err => {
+    if (err) {
+      console.error(err);
+      return res(err);
     } else {
-      console.log("Couldn't connect to db");
+      mongoClient.db(MONGO_DB_NAME).collection(DISTANCES_COLLECTION).find().toArray((err, result) => {
+        if (err) {
+          console.error(err);
+          return res(err);
+        } else {
+          console.dir(result);
+          return res.json(result);
+        }
+      });
+      mongoClient.close();
     }
   });
+});
+app.post('/distance', (req, res) => {
+  res.send('POST /distance');
+});
+
+app.get('/uv', (req, res) => {
+  res.send('GET /uv');
+});
+app.post('/uv', (req, res) => {
+  res.send('POST /uv');
+});
+
+app.get('/heart', (req, res) => {
+  res.send('GET /heart');
+});
+app.post('/heart', (req, res) => {
+  res.send('POST /heart');
+});
+
+app.get('/tilt', (req, res) => {
+  res.send('GET /tilt');
+});
+app.post('/tilt', (req, res) => {
+  res.send('POST /tilt');
+});
+
+app.get('/bottle', (req, res) => {
+  res.send('GET /bottle');
+});
+app.post('/bottle', (req, res) => {
+  res.send('POST /bottle');
+});
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`HTTP server started on port: ${PORT}`)
+});
