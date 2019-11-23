e544db528fd151fe3bc24c9855a8d5b0aaff55c7
Alex Kubica <alexkubicail@gmail.com>
2019-11-23 17:13:45 +0200

    implement bottle gauge in client

--- web-client/src/BottleGraph.js
+++ web-client/src/BottleGraph.js
@@ -1,175 +1,208 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import moment from 'moment';
-import Highcharts from 'highcharts';
-import highchartsMore from 'highcharts/highcharts-more';
-import solidgauge from 'highcharts/modules/solid-gauge';
-import HighchartsReact from 'highcharts-react-official';
-import { DataPropType, MINUTES_IN_MS, graphSubtitleText, GRAPH_DATE_FORMAT } from './utils';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import moment from "moment";
+import Highcharts from "highcharts";
+import highchartsMore from "highcharts/highcharts-more";
+import solidgauge from "highcharts/modules/solid-gauge";
+import HighchartsReact from "highcharts-react-official";
+import {
+  DataPropType,
+  MINUTES_IN_MS,
+  graphSubtitleText,
+  GRAPH_DATE_FORMAT
+} from "./utils";
 
 highchartsMore(Highcharts);
 solidgauge(Highcharts);
 
 const propTypes = {
-	data: PropTypes.arrayOf(DataPropType),
+  data: PropTypes.arrayOf(DataPropType)
 };
 
 export default function BottleGraph({ data }) {
-	const lineOptions = {
-		chart: {
-			zoomType: 'x',
-			panning: true,
-			panKey: 'shift',
-		},
-		title: {
-			text: 'Water consumption over time',
-		},
-		subtitle: {
-			text: graphSubtitleText(),
-		},
-		xAxis: {
-			type: 'datetime',
-			minRange: 5 * MINUTES_IN_MS,
-		},
-		yAxis: {
-			title: {
-				text: 'Bottle water capacity',
-			},
-			labels: {
-				format: '{value}%',
-			},
-			min: 0,
-			max: 100,
-		},
-		tooltip: {
-			xDateFormat: GRAPH_DATE_FORMAT,
-			valueSuffix: '%',
-		},
-		legend: {
-			enabled: false,
-		},
-		plotOptions: {
-			area: {
-				fillColor: {
-					linearGradient: {
-						x1: 0,
-						y1: 0,
-						x2: 0,
-						y2: 1,
-					},
-					stops: [
-						[0, Highcharts.getOptions().colors[0]],
-						[
-							1,
-							Highcharts.Color(Highcharts.getOptions().colors[0])
-								.setOpacity(0)
-								.get('rgba'),
-						],
-					],
-				},
-				marker: {
-					radius: 2,
-				},
-				lineWidth: 1,
-				states: {
-					hover: {
-						lineWidth: 1,
-					},
-				},
-				threshold: null,
-			},
-		},
-		series: [
-			{
-				type: 'spline',
-				name: 'Bottle',
-				data: data.map(obj => {
-					return [moment(obj.timestamp).valueOf(), obj.value];
-				}),
-			},
-		],
-	};
-	const gaugeOptions = {
-		chart: {
-			type: 'solidgauge',
-		},
+  const [currentBottle, setCurrentBottle] = useState();
 
-		pane: {
-			center: ['50%', '85%'],
-			size: '140%',
-			startAngle: -90,
-			endAngle: 90,
-			background: {
-				backgroundColor: Highcharts.defaultOptions.legend.backgroundColor || '#EEE',
-				innerRadius: '60%',
-				outerRadius: '100%',
-				shape: 'arc',
-			},
-		},
+  const lineOptions = {
+    chart: {
+      zoomType: "x",
+      panning: true,
+      panKey: "shift"
+    },
+    title: {
+      text: "Water consumption over time"
+    },
+    subtitle: {
+      text: graphSubtitleText()
+    },
+    xAxis: {
+      type: "datetime",
+      minRange: 5 * MINUTES_IN_MS
+    },
+    yAxis: {
+      title: {
+        text: "Bottle water capacity"
+      },
+      labels: {
+        format: "{value}%"
+      },
+      min: 0,
+      max: 100
+    },
+    tooltip: {
+      xDateFormat: GRAPH_DATE_FORMAT,
+      valueSuffix: "%"
+    },
+    legend: {
+      enabled: false
+    },
+    plotOptions: {
+      area: {
+        fillColor: {
+          linearGradient: {
+            x1: 0,
+            y1: 0,
+            x2: 0,
+            y2: 1
+          },
+          stops: [
+            [0, Highcharts.getOptions().colors[0]],
+            [
+              1,
+              Highcharts.Color(Highcharts.getOptions().colors[0])
+                .setOpacity(0)
+                .get("rgba")
+            ]
+          ]
+        },
+        marker: {
+          radius: 2
+        },
+        lineWidth: 1,
+        states: {
+          hover: {
+            lineWidth: 1
+          }
+        },
+        threshold: null
+      },
+      series: {
+        point: {
+          events: {
+            mouseOver: function() {
+              setCurrentBottle({
+                capacity: this.y,
+                time: this.x
+              });
+            },
+            mouseOut: function() {
+              setCurrentBottle(-1);
+            }
+          }
+        }
+      }
+    },
+    series: [
+      {
+        type: "spline",
+        name: "Bottle",
+        data: data.map(obj => {
+          return [moment(obj.timestamp).valueOf(), obj.value];
+        })
+      }
+    ]
+  };
+  const gaugeOptions = {
+    chart: {
+      type: "solidgauge"
+    },
 
-		tooltip: {
-			enabled: false,
-		},
+    title: {
+      text: currentBottle
+        ? `Bottle capacity in ${moment(currentBottle.time).format(
+            "DD/MM/YYYY HH:mm:ss"
+          )}`
+        : `Hover over a point to see the gauge change`
+    },
 
-		// the value axis
-		yAxis: {
-			stops: [
-				[0.1, '#55BF3B'], // green
-				[0.5, '#DDDF0D'], // yellow
-				[0.9, '#DF5353'], // red
-			],
-			lineWidth: 0,
-			minorTickInterval: null,
-			tickAmount: 2,
-			labels: {
-				y: 16,
-			},
-			min: 0,
-			max: 200,
-			title: {
-				y: -70,
-				text: 'Bottle ',
-			},
-		},
+    pane: {
+      startAngle: -90,
+      endAngle: 90,
+      background: {
+        backgroundColor:
+          Highcharts.defaultOptions.legend.backgroundColor || "#EEE",
+        innerRadius: "60%",
+        outerRadius: "100%",
+        shape: "arc"
+      }
+    },
 
-		plotOptions: {
-			solidgauge: {
-				dataLabels: {
-					y: 5,
-					borderWidth: 0,
-					useHTML: true,
-				},
-			},
-		},
+    tooltip: {
+      enabled: false
+    },
 
-		credits: {
-			enabled: false,
-		},
+    // the value axis
+    yAxis: {
+      stops: [
+        [0.1, "#55BF3B"], // green
+        [0.5, "#DDDF0D"], // yellow
+        [0.9, "#DF5353"] // red
+      ],
+      lineWidth: 0,
+      minorTickInterval: null,
+      tickAmount: 2,
+      labels: {
+        y: 16
+      },
+      min: 0,
+      max: 100,
+      title: {
+        y: -90,
+        text: "Bottle capacity"
+      }
+    },
 
-		series: [
-			{
-				name: 'Speed',
-				data: [80],
-				dataLabels: {
-					format:
-						'<div style="text-align:center">' +
-						'<span style="font-size:25px">{y}</span><br/>' +
-						'<span style="font-size:12px;opacity:0.4">km/h</span>' +
-						'</div>',
-				},
-				tooltip: {
-					valueSuffix: ' km/h',
-				},
-			},
-		],
-	};
+    plotOptions: {
+      solidgauge: {
+        dataLabels: {
+          y: 5,
+          borderWidth: 0,
+          useHTML: true
+        },
+        animation: {
+          duration: 200
+        }
+      }
+    },
 
-	return (
-		<div>
-			<HighchartsReact highcharts={Highcharts} options={lineOptions} />
-			<HighchartsReact highcharts={Highcharts} options={gaugeOptions} />
-		</div>
-	);
+    credits: {
+      enabled: false
+    },
+
+    series: [
+      {
+        name: "Bottle capacity",
+        data: currentBottle ? [currentBottle.capacity] : [],
+        dataLabels: {
+          format:
+            '<div style="text-align:center">' +
+            '<span style="font-size:25px">{y}%</span><br/>' +
+            "</div>"
+        },
+        tooltip: {
+          valueSuffix: "%"
+        }
+      }
+    ]
+  };
+
+  return (
+    <div>
+      <div style={{ marginBottom: "50px" }}>
+        <HighchartsReact highcharts={Highcharts} options={lineOptions} />
+      </div>
+      <HighchartsReact highcharts={Highcharts} options={gaugeOptions} />
+    </div>
+  );
 }
 
 BottleGraph.propTypes = propTypes;
--- web-client/src/PostureGraph.js
+++ web-client/src/PostureGraph.js
@@ -1,92 +1,97 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import moment from 'moment';
-import Highcharts from 'highcharts';
-import HighchartsReact from 'highcharts-react-official';
-import { DataPropType, MINUTES_IN_MS, graphSubtitleText, GRAPH_DATE_FORMAT } from './utils';
+import React from "react";
+import PropTypes from "prop-types";
+import moment from "moment";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import {
+  DataPropType,
+  MINUTES_IN_MS,
+  graphSubtitleText,
+  GRAPH_DATE_FORMAT
+} from "./utils";
 
 const propTypes = {
-	data: PropTypes.arrayOf(DataPropType),
+  data: PropTypes.arrayOf(DataPropType)
 };
 
 export default function PostureGraph({ data }) {
-	const options = {
-		chart: {
-			zoomType: 'x',
-			panning: true,
-			panKey: 'shift',
-		},
-		title: {
-			text: 'Posture angle over time',
-		},
-		subtitle: {
-			text: graphSubtitleText(),
-		},
-		xAxis: {
-			type: 'datetime',
-			minRange: 5 * MINUTES_IN_MS,
-		},
-		yAxis: {
-			title: {
-				text: 'Posture angle',
-			},
-			labels: {
-				format: '{value}°',
-			},
-			min: 0,
-			max: 360,
-			tickPositions: [0, 90, 180, 270, 360],
-		},
-		tooltip: {
-			xDateFormat: GRAPH_DATE_FORMAT,
-			valueSuffix: '°',
-		},
-		legend: {
-			enabled: false,
-		},
-		plotOptions: {
-			area: {
-				fillColor: {
-					linearGradient: {
-						x1: 0,
-						y1: 0,
-						x2: 0,
-						y2: 1,
-					},
-					stops: [
-						[0, Highcharts.getOptions().colors[0]],
-						[
-							1,
-							Highcharts.Color(Highcharts.getOptions().colors[0])
-								.setOpacity(0)
-								.get('rgba'),
-						],
-					],
-				},
-				marker: {
-					radius: 2,
-				},
-				lineWidth: 1,
-				states: {
-					hover: {
-						lineWidth: 1,
-					},
-				},
-				threshold: null,
-			},
-		},
-		series: [
-			{
-				type: 'spline',
-				name: 'Posture',
-				data: data.map(obj => {
-					return [moment(obj.timestamp).valueOf(), obj.value];
-				}),
-			},
-		],
-	};
+  const options = {
+    chart: {
+      zoomType: "x",
+      panning: true,
+      panKey: "shift"
+    },
+    title: {
+      text: "Posture angle over time"
+    },
+    subtitle: {
+      text: graphSubtitleText()
+    },
+    xAxis: {
+      type: "datetime",
+      minRange: 5 * MINUTES_IN_MS
+    },
+    yAxis: {
+      title: {
+        text: "Posture angle"
+      },
+      labels: {
+        format: "{value}°"
+      },
+      min: 0,
+      max: 360,
+      tickPositions: [0, 90, 180, 270, 360]
+    },
+    tooltip: {
+      xDateFormat: GRAPH_DATE_FORMAT,
+      valueSuffix: "°"
+    },
+    legend: {
+      enabled: false
+    },
+    plotOptions: {
+      area: {
+        fillColor: {
+          linearGradient: {
+            x1: 0,
+            y1: 0,
+            x2: 0,
+            y2: 1
+          },
+          stops: [
+            [0, Highcharts.getOptions().colors[0]],
+            [
+              1,
+              Highcharts.Color(Highcharts.getOptions().colors[0])
+                .setOpacity(0)
+                .get("rgba")
+            ]
+          ]
+        },
+        marker: {
+          radius: 2
+        },
+        lineWidth: 1,
+        states: {
+          hover: {
+            lineWidth: 1
+          }
+        },
+        threshold: null
+      }
+    },
+    series: [
+      {
+        type: "spline",
+        name: "Posture",
+        data: data.map(obj => {
+          return [moment(obj.timestamp).valueOf(), obj.value];
+        })
+      }
+    ]
+  };
 
-	return <HighchartsReact highcharts={Highcharts} options={options} />;
+  return <HighchartsReact highcharts={Highcharts} options={options} />;
 }
 
 PostureGraph.propTypes = propTypes;
